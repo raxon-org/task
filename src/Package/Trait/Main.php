29d3be74d7a8af9d263e175fcdcea086384b5efa
remco van der Velde <remco.pc@outlook.com>
2025-01-30 17:48:27 +0100

    task package

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -3,6 +3,7 @@ namespace Package\Raxon\Task\Trait;
 
 use Raxon\App;
 
+use Raxon\Exception\ObjectException;
 use Raxon\Module\Core;
 use Raxon\Module\File;
 
@@ -21,9 +22,25 @@ trait Main {
         echo 'Install ' . $object->request('package') . '...' . PHP_EOL;
     }
 
+    /**
+     * @throws ObjectException
+     */
     public function task_create($flags, $options): void
     {
         $object = $this->object();
+
+        if(property_exists($options, 'user')){
+            $class = 'Account.User';
+            $node = new Node($object);
+            $where = [];
+            foreach($options->user as $property => $value){
+                $where[$property] = $value;
+            }
+            $record = $node->record($class, $node->role_system(), ['where' => $where]);
+            breakpoint($record);
+        }
+
+
         d($options);
         echo 'node';
         breakpoint($flags);
