277367b0fd194bd7b9e5a1f3e623ee5a9b1a6fc3
remco van der Velde <remco.pc@outlook.com>
2025-05-02 07:58:11 +0200

    implementing new parser

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -25,7 +25,7 @@ trait Main {
         $object = $this->object();
         echo 'Install ' . $object->request('package') . '...' . PHP_EOL;
         $schema_url = $object->config('project.dir.package') . 'Raxon/Task/Schema/Task.json';
-        if(property_exists('connection', $options)){
+        if(property_exists($options, 'connection')){
             $schema_connection = $options->connection;
         } else {
             $schema_connection = $object->config('doctrine.environment.system.*.uuid');
