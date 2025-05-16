f8d1416c081bba9ae5efe19af0552f5ce9d390d4
remco van der Velde <remco.pc@outlook.com>
2025-05-16 18:02:23 +0200

    info output

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -39,7 +39,6 @@ trait Service {
         $node = new Node($object);
         $role = $node->role_system();
         $object->request('filter.status', 'Pending');
-        ddd($object->request());
         $list = Entity::list($object,$connection->manager, $role, $entity, $options);
         return $list;
     }
