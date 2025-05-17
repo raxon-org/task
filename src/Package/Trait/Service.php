6ccba02fd4c02ea103c11b4cd36b8ebf195853dc
remco van der Velde <remco.pc@outlook.com>
2025-05-17 09:00:05 +0200

    info output

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -204,7 +204,7 @@ trait Service {
                 $url_stderr = $dir_stderr . $record['node']['uuid'];
                 foreach($record['node']['command'] as $nr => $command){
                     $command = $command . ' > ' . $url_stdout . ' 2> ' . $url_stderr . ' &';
-                    exec($command . ' 2>&1', $output, $code);
+                    exec($command, $output, $code);
                     echo $output;
 //                    echo $url_stdout . PHP_EOL;
 //                    echo $url_stderr . PHP_EOL;
