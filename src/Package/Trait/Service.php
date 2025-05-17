a8054bb0c9de03c7af0c75378f584026f9303af8
remco van der Velde <remco.pc@outlook.com>
2025-05-17 08:25:10 +0200

    info output

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -173,8 +173,7 @@ trait Service {
         $object->request('entity', $entity);
         $object->request('filter.status', Status::PENDING);
         $object->request('order.isCreated', 'ASC');
-
-        $options->page = 2;
+        $object->request('page', 2);
 
 
         $record = Entity::record($object,$connection->manager, $role, $options);
