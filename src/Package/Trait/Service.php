90587eaacd84d8ce4cc71e4972574c5b96405172
Remco van der Velde <remco@universeorange.com>
2026-04-26 15:02:12 +0200

    init

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -211,7 +211,7 @@ trait Service {
     {
         //Dir::create(/tmp/raxon/org/726ed3f2-896d-4241-bf26-0b69f2911f81/33/);
         $object = $this->object();
-        ddd($object->config('project.dir'));
+        ddd($object->config('ramdisk.url'));
 //        Dir::create($object->config('project.dir.temp'));
 
         $config = Database::config($object);
